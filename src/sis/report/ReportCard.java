100ae175b34bfa4cfe258d85b9ccc04eed13e535
cuixiaorui <cui_xiaorui@126.com>
2020-01-30 18:12:49 +0800

    refactor: #6-2-1 extract method

--- src/sis/report/ReportCard.java
+++ src/sis/report/ReportCard.java
@@ -18,12 +18,16 @@ public class ReportCard {
 
     private Map<Student.Grade, String> getMessage(){
         if(message == null){
-            message = new EnumMap<Student.Grade, String>(Student.Grade.class);
-            message.put(Student.Grade.A,ReportCard.A_MESSAGE);
-            message.put(Student.Grade.B,ReportCard.B_MESSAGE);
-            message.put(Student.Grade.C,ReportCard.C_MESSAGE);
-            message.put(Student.Grade.D,ReportCard.D_MESSAGE);
+            initMessage();
         }
         return message;
+    }
+
+    private void initMessage() {
+        message = new EnumMap<Student.Grade, String>(Student.Grade.class);
+        message.put(Student.Grade.A, ReportCard.A_MESSAGE);
+        message.put(Student.Grade.B,ReportCard.B_MESSAGE);
+        message.put(Student.Grade.C,ReportCard.C_MESSAGE);
+        message.put(Student.Grade.D,ReportCard.D_MESSAGE);
     }
 }
