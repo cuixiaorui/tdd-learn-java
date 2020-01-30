2c716fa70c646f7d7847ba4a93e1d8f045b8c27a
cuixiaorui <cui_xiaorui@126.com>
2020-01-30 17:54:18 +0800

    feat: #6-1 switch

--- src/sis/studentInfo/HonorsGraddingStrategy.java
+++ src/sis/studentInfo/HonorsGraddingStrategy.java
@@ -3,10 +3,18 @@ package sis.studentInfo;
 public class HonorsGraddingStrategy implements GraddingStrategy{
     public int getGradePointsFor(Student.Grade grade)
     {
-        if(grade.equals(Student.Grade.A))return 5;
-        if(grade.equals(Student.Grade.B))return 4;
-        if(grade.equals(Student.Grade.C))return 3;
-        if(grade.equals(Student.Grade.D))return 2;
-        return 0;
+        int point = bacisGradePointsFor(grade);
+        return point+=1;
+    }
+
+    private int bacisGradePointsFor(Student.Grade grade){
+        switch (grade)
+        {
+            case A: return 4;
+            case B: return 3;
+            case C: return 2;
+            case D: return 1;
+            default: return 0;
+        }
     }
 }
