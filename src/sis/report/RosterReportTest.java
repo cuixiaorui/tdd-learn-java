205aa8c6ebef2ec6fb33d72c76a290be4ca0de48
cuixiaorui <cui_xiaorui@126.com>
2020-02-15 18:28:52 +0800

    test: delete file after testing is completed

--- src/sis/report/RosterReportTest.java
+++ src/sis/report/RosterReportTest.java
@@ -33,18 +33,33 @@ public class RosterReportTest extends TestCase {
 
     public void testFiledReport() throws IOException {
         final String filename = "testFiledReport.txt";
-        new RosterReport(session).writeReport(filename);
 
-        StringBuffer buffer = new StringBuffer();
-        String line;
+        try {
+            delete(filename);
+            new RosterReport(session).writeReport(filename);
 
-        BufferedReader reader = new BufferedReader(new FileReader(filename));
-        while ((line = reader.readLine()) != null) {
-            buffer.append(line);
-            buffer.append(RosterReport.NEW_LINE);
+            StringBuffer buffer = new StringBuffer();
+            String line;
+
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line);
+                buffer.append(RosterReport.NEW_LINE);
+            }
+            reader.close();
+            assertReportContents(buffer.toString());
         }
-        reader.close();
-        assertReportContents(buffer.toString());
+        finally {
+            delete(filename);
+        }
+    }
+
+    private void delete(String filename) {
+        File file = new File(filename);
+        if (file.exists()) {
+            boolean result = file.delete();
+            assertTrue("unable to delete" + filename, result);
+        }
     }
 
     private void assertReportContents(String rosterReport) {
